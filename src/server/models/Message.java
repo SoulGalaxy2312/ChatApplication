55188abfdc58123b5fda0754f820786235c5dc88
SoulGalaxy2312 <ngotri2312@gmail.com>
2024-12-10 14:36:20 +0700

    Message model

---
+++ src/server/models/Message.java
@@ -0,0 +1,46 @@
+package server.models;
+
+enum MessageType {
+    TEXT,
+    FILE
+};
+
+public class Message {
+    private String sender;
+    private String receiver;
+    private MessageType type;
+    private byte[] content;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content != null ? content.clone() : null;
+    }
+}
+
